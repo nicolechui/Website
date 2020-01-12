e592f107387eedd4313a3b1d13222a69660bbf37
Matthew Braun <mbraun5@csu.fullerton.edu>
2020-01-12 03:35:35 -0800

    modified animation for split components

--- scripts/root.js
+++ scripts/root.js
@@ -1,61 +1,57 @@
 var visible;
 var currLetter;
-var colorIndex;
 var underscore_func;
 var text_func;
+var next_sequence_flag = true;
 
 var colors = ["purple", "red"];
 
-function switch_underscore() {
-    var underscore = document.getElementById("underscore");
-
-    if (visible === true) {
-        underscore.className = "text-underscore hidden";
-        visible = false;
-    }
-    else {
-        underscore.className = "text-underscore";
-        visible = true;
-    }
-}
-
-function write_next(target, text) {
-    target.innerHTML = text.substring(0, currLetter);
-    if (text[currLetter] == " ") {
-        colorIndex = (colorIndex + 1) % colors.length;
-        target.setAttribute("style", "color:" + colors[colorIndex]);
-    }
-    currLetter += 1;
-    if (currLetter > text.length) {
-        window.clearInterval(text_func);
-        window.clearInterval(underscore_func);
-
-        // Turn off underscore
-        visible = true;
-        // var underscore = document.getElementById("underscore")    Destroy underscore rather than changing opacity to 0.
-        // underscore.parentNode.removeChild(underscore);
-        switch_underscore();
-    }
-}
-
-function typewrite(tag){
-    currLetter = 1;
-    colorIndex = 0;
+function switch_underscore(target) {
+  if (visible === true) {
+    target.className = "text-underscore hidden";
+    visible = false;
+  }
+  else {
+    target.className = "text-underscore";
     visible = true;
-
-    var target = document.getElementById(tag);
-    var text = target.innerHTML;
-
-    target.setAttribute("style", "color:" + colors[colorIndex]);
-    underscore_func = window.setInterval(switch_underscore, 250);
-    text_func = window.setInterval(write_next, 120, target, text);
-
-    document.write(text);
+  }
 }
 
-typewrite("text");
-//typewrite("text_two"); modify typewrite function to take parameters text_tag and underscore_tag so we can typewrite these sequentially.
+function write_next(target, text, underscore_target) {
+  target.innerHTML = text.substring(0, currLetter);
+  currLetter += 1;
+  if (currLetter > text.length) {
+    window.clearInterval(text_func);
+    window.setTimeout(next_sequence, 1000, underscore_target);
+  }
+}
 
+function next_sequence(underscore_target) {
+  window.clearInterval(underscore_func);
+  underscore_target.parentNode.removeChild(underscore_target);
+
+  if (next_sequence_flag) {
+    typewrite("text_two", "underscore_two", colors[1]);
+    next_sequence_flag = false;
+  }
+}
+
+function typewrite(text_tag, underscore_tag, color){
+  currLetter = 1;
+  colorIndex = 0;
+  visible = true;
+
+  var text_target = document.getElementById(text_tag);
+  var underscore_target = document.getElementById(underscore_tag);
+  var text = text_target.innerHTML;
+
+  text_target.setAttribute("style", "color:" + color);
+  underscore_target.setAttribute("style", "color:" + color);
+  underscore_func = window.setInterval(switch_underscore, 300, underscore_target);
+  text_func = window.setInterval(write_next, 120, text_target, text, underscore_target);
+}
+
+typewrite("text", "underscore", colors[0]);
 
 function move_to_loc(tag) {
     var target = document.getElementById(tag);
