56d679a19b34ab7565d4b98936645aca50a2a281
Matthew Braun <mbraun5@csu.fullerton.edu>
2020-02-29 18:59:02 -0800

    added experience text styling and button shifting

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,13 +1,21 @@
 var active_button = document.getElementById("experience-btn-group").children[0];
+var active_text = document.getElementById("experience-txt-group").children[0];
 active_button.classList.toggle("active-button");
+active_text.classList.toggle("active-text");
 
-function toggle_active(btn) {
+function toggle_active(btn, idx) {
     if (btn != active_button) {
         active_button.classList.toggle("active-button");
         btn.classList.toggle("active-button");
+
+        active_text.classList.toggle("active-text")
+        active_text = document.getElementById("experience-txt-group").children[idx];
+        active_text.classList.toggle("active-text");
+
         active_button = btn;
     }
 }
+
 function toggle_dropdown(menu) {
     menu.classList.toggle("change");
 }
